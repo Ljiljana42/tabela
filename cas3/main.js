abff764d1fd338c59a6f6a0de1ef6f0f13249148
Ljiljana42 <64222291+Ljiljana42@users.noreply.github.com>
2020-04-25 21:17:33 +0200

    Add files via upload

--- cas3/main.js
+++ cas3/main.js
@@ -1,31 +1,17 @@
-let player1=prompt("Player 1 name");
-let player2=prompt("Player 2 name");
-let round=0;
-play1Res=0;
-play2Res=0;
+let myLi=document.getElementsByTagName("li");
 
-let igra=setInterval(function (){
-round++;
-console.log("***ROUND "+round+"***");
-let play1=Math.ceil((Math.random()*6));
-let play2=Math.ceil((Math.random()*6));
-console.log(player1+":" + play1 +"--------" +player2+":"+play2);
-if (play1>play2){
-  play1Res++;
-} else if (play1<play2) {play2Res++
- };
-    if (round===10 && play1Res>play2Res){
-      console.log("REZULTAT " + player1+":"+play1Res+ "-----"+ player2+":"+ play2Res)
-      console.log("WINNER is " + player1 + "!")}
-    else if (round===10 && play1Res<play2Res){
-      console.log("REZULTAT "+ player1+":"+play1Res+ "-----"+ player2+":"+play2Res)
-      console.log("WINNER is " + player2 + "!")}
-    else if (round===10 && play1Res===play2Res){
-        console.log("REZULTAT "+ player1+":"+play1Res+ "-----"+ player2+":"+play2Res)
-        console.log("DRAW!")}
+let counter=0;
+let i=0;
 
-},1000);
+let colouring= setInterval(function(){
 
-setTimeout(function(){
-  clearInterval(igra)
-},10000);
+    for(counter=0;counter<myLi.length;counter++){
+        myLi[counter].style.background="transparent";  
+       }
+    
+    myLi[i].style.background="tomato";
+    i++;
+        if(i===myLi.length){
+        i=0;
+        }
+},1000)
